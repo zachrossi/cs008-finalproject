992c7e5c02d7d641e0f281c1db91f9d50021ec7d
zachrossi <57643983+zachrossi@users.noreply.github.com>
2019-11-20 10:04:04 -0500

    index file

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<!--
+Index.php
+Home Page
+
+maybe add:
+-search bar
+-gallery
+-->
+<?php
+include ("top.php");
+include ("nav.php");
+?>
+<main>
+    <article>
+        <h1>PlanetBay</h1>
+        <!--Figure 1-->
+        <section>
+            <figure>
+                <figcaption></figcaption>
+            </figure>
+        </section>
+        <!--Figure 2-->
+        <section>
+            <figure>
+                <figcaption></figcaption>
+            </figure>
+        </section>
+        <!--Figure 3-->
+        <section>
+            <figure>
+                <figcaption></figcaption>
+            </figure>
+        </section>
+        <!--Figure 4-->
+        <section>
+            <figure>
+                <figcaption></figcaption>
+            </figure>
+        </section>
+    </article>
+    <?php
+    include ("footer.php");
+    ?>
+</main>
