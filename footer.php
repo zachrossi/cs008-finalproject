27c53f10a1e6b85d743956db7863cae5eefea169
jldeland <57643954+jldeland@users.noreply.github.com>
2019-11-20 17:48:39 -0500

    footer.php -> Updated order

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@ footer
 <footer class="footer_paragraph">
     <a href="index.php">Home</a>
     <a href="deals.php">Deals</a>
-    <a href="about.php">About</a>
-    <a href="products.php">Products</a>
+    <a href="products.php">Shop Planets</a>
     <a href="contest.php">Contest</a>
+    <a href="about.php">About</a>
 </footer>
