b5235b872c8cd04d7a811c93a3dec1152fb09653
jldeland <57643954+jldeland@users.noreply.github.com>
2019-11-20 00:28:03 -0500

    Created an example template for a product details page
    I will continue to update this with more content. Feel free to add CSS to it so we can have a template w/ CSS and HTML ready to be used easily with our actual products

---
+++ genericProductPage.php
@@ -0,0 +1,26 @@
+<?php
+    include "top.php";
+    include "nav.php";
+?>
+
+<main>
+
+    <h1 class = "planetName">Planet Name</h1>
+
+    <h3 class = "rating">5 Stars based on 45 Reviews</h3>
+
+    <h2 class = "price">$1,240,000</h2>
+
+    <figure class = "planetImages">
+        <img src="finalProjectImages/genericplanet.jpg" alt="This should show the main planet image">
+        <!-- Under the main image will be all the images in a row, smaller than the main image -->
+    </figure>
+
+    <p class = "description">This is where the planet will be described in great detail.
+    We can also add a list of the main features if we want, and maybe some other info</p>
+
+    <h2 class = "purchaseButton">Purchase Planet</h2>
+
+</main>
+
+<?php include "footer.php" ?>
