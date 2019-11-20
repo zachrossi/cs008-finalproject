03fbc92a2baea4345e452494587018e31144ce98
jldeland <57643954+jldeland@users.noreply.github.com>
2019-11-20 17:52:42 -0500

    Updated titles

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@ footer
 -->
 <footer class="footer_paragraph">
     <a href="index.php">Home</a>
-    <a href="deals.php">Deals</a>
+    <a href="deals.php">Today's Deals</a>
     <a href="products.php">Shop Planets</a>
     <a href="contest.php">Contest</a>
     <a href="about.php">About</a>
