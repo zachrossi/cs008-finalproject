92440f029673023d053c87cfd5a5460a1d11f4f7
zachrossi <57643983+zachrossi@users.noreply.github.com>
2019-11-26 11:40:40 -0500

    Added image urls and classes

--- index.php
+++ index.php
@@ -15,27 +15,31 @@ include ("nav.php");
     <article>
         <h1>PlanetBay</h1>
         <!--Figure 1-->
-        <section>
+        <section class="section_one">
             <figure>
-                <figcaption></figcaption>
+                <img src="planets/planet1.png" alt="Image of planet 1">
+                <figcaption>Planet 1</figcaption>
             </figure>
         </section>
         <!--Figure 2-->
-        <section>
+        <section class="section_two">
             <figure>
-                <figcaption></figcaption>
+                <img src="planets/planet2.jpg" alt="Image of planet 2">
+                <figcaption>Planet 2</figcaption>
             </figure>
         </section>
         <!--Figure 3-->
-        <section>
+        <section class="section_three">
             <figure>
-                <figcaption></figcaption>
+                <img src="planets/planet3.jpg" alt="Image of planet 3">
+                <figcaption>planet 3</figcaption>
             </figure>
         </section>
         <!--Figure 4-->
-        <section>
+        <section class="section_four">
             <figure>
-                <figcaption></figcaption>
+                <img src="planets/planet4.jpg" alt="Image of planet 4">
+                <figcaption>planet 4</figcaption>
             </figure>
         </section>
     </article>
@@ -43,4 +47,4 @@ include ("nav.php");
     include ("footer.php");
     ?>
 </main>
-</html>
+
