f0bfa27235bb7a83a273421b65ff081cfc820158
zachrossi <57643983+zachrossi@users.noreply.github.com>
2019-11-26 12:36:37 -0500

    updated files

---
+++ about.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<!--
+about.php
+
+add
+-image
+-->
+<?php
+include ("top.php");
+include ("nav.php");
+?>
+<main>
+    <article>
+        <section>
+            <h1>About Us</h1>
+            <p>Insert text about maybe 500ish words</p>
+        </section>
+        <!--Picture of logo, maybe-->
+        <section>
+            <figure>
+                <figcaption></figcaption>
+            </figure>
+        </section>
+        <section>
+            <h2>Contact Us</h2>
+            <address>67 Star Avenue, Moon Colony 6</address>
+            <p>Phone Number: (666) 666 6661</p>
+            <p>Email: contact@planetbay.com</p>
+        </section>
+    </article>
+    <?php
+    include ("footer.php");
+    ?>
+</main>
--- top.php
+++ top.php
@@ -1,3 +1,17 @@
+<?php
+
+$phpSelf = htmlentities($_SERVER['PHP_SELF'], ENT_QUOTES, "UTF-8");
+
+$path_parts = pathinfo($phpSelf);
+
+$databaseName = 'ZROSSI_finalProject';
+$dsn = 'mysql:host=webdb.uvm.edu;dbname=' . $databaseName;
+$dbUserName = "zrossi_writer"; 
+$dbPassword = "MDdC3cSqx2PJbgbF";  // as listed in the original email when you create your account
+
+print '<!-- Make DB connection -->';
+$pdo = new PDO($dsn, $dbUserName, $dbPassword);
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -31,9 +45,13 @@
         }
         //add
         //Extra if we need
+        if ($path_parts['filename'] == "review") {
+            print 'PlanetBay -  Review | Zachary Rossi, Tyler Eisenmenger, Josh Deland</title> ';
+            print '<meta name="description" content="This includes the page to review a planet">';
+        }
         ?>        
         <meta charset="UTF-8">
-        <meta name="author" content="Zachary Rossi, Tyler Eisenmenger, Josh Deland">
+        <meta name="author" content="Zachary Rossi">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="css/custon.css?version=1.0" type="text/css">
     </head>
