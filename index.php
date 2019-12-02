43bfd3eaa95ed849d22293ae4a52f2e4fac1a459
jldeland <57643954+jldeland@users.noreply.github.com>
2019-12-02 12:39:39 -0500

    Edited image pathways

--- index.php
+++ index.php
@@ -17,28 +17,28 @@ include ("nav.php");
         <!--Figure 1-->
         <section class="section_one">
             <figure>
-                <img src="planets/planet1.png" alt="Image of planet 1">
+                <img src="images/planets/planet1.png" alt="Image of planet 1">
                 <figcaption>Planet 1</figcaption>
             </figure>
         </section>
         <!--Figure 2-->
         <section class="section_two">
             <figure>
-                <img src="planets/planet2.jpg" alt="Image of planet 2">
+                <img src="images/planets/planet2.jpg" alt="Image of planet 2">
                 <figcaption>Planet 2</figcaption>
             </figure>
         </section>
         <!--Figure 3-->
         <section class="section_three">
             <figure>
-                <img src="planets/planet3.jpg" alt="Image of planet 3">
+                <img src="images/planets/planet3.jpg" alt="Image of planet 3">
                 <figcaption>planet 3</figcaption>
             </figure>
         </section>
         <!--Figure 4-->
         <section class="section_four">
             <figure>
-                <img src="planets/planet4.jpg" alt="Image of planet 4">
+                <img src="images/planets/planet4.jpg" alt="Image of planet 4">
                 <figcaption>planet 4</figcaption>
             </figure>
         </section>
