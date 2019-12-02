1fb278d7b340216de8efc2cb7cd96d45bcc6353e
jldeland <57643954+jldeland@users.noreply.github.com>
2019-12-01 21:03:49 -0500

    Add page for "shop planets" nav link

---
+++ allplanets.php
@@ -0,0 +1,59 @@
+<?php
+include "top.php";
+include "nav.php";
+?>
+<link rel="stylesheet" href="css/allplanets.css" TYPE="text/css">
+<main>
+    <h1>Shop All Planets</h1>
+    <h2>View our entire selection of planets for sale. Select a planet for more details.</h2>
+
+    <section class="planetGrid">
+        <figure class="planet1">
+            <img src="planets/planet1.png">
+            <figcaption>Planet 1</figcaption>
+        </figure>
+
+        <figure class="planet2">
+            <img src="planets/planet1.png">
+            <figcaption>Planet 2</figcaption>
+        </figure>
+
+        <figure class="planet3">
+            <img src="planets/planet1.png">
+            <figcaption>Planet 3</figcaption>
+        </figure>
+
+        <figure class="planet4">
+            <img src="planets/planet1.png">
+            <figcaption>Planet 4</figcaption>
+        </figure>
+
+        <figure class="planet5">
+            <img src="planets/planet1.png">
+            <figcaption>Planet 5</figcaption>
+        </figure>
+
+        <figure class="planet6">
+            <img src="planets/planet1.png">
+            <figcaption>Planet 6</figcaption>
+        </figure>
+
+        <figure class="planet7">
+            <img src="planets/planet1.png">
+            <figcaption>Planet 7</figcaption>
+        </figure>
+
+        <figure class="planet8">
+            <img src="planets/planet1.png">
+            <figcaption>Planet 8</figcaption>
+        </figure>
+
+        <figure class="planet9">
+            <img src="planets/planet1.png">
+            <figcaption>Planet 9</figcaption>
+        </figure>
+    </section>
+</main>
+
+
+<?php include "footer.php"; ?>
