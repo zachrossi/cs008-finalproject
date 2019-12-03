43465bcec2c25b970e9d3c4ac0086eef41bdeeeb
jldeland <57643954+jldeland@users.noreply.github.com>
2019-12-03 00:02:46 -0500

    Updated with 9 planets and images

--- allplanets.php
+++ allplanets.php
@@ -2,58 +2,58 @@
 include "top.php";
 include "nav.php";
 ?>
-<link rel="stylesheet" href="css/allplanets.css" TYPE="text/css">
-<main>
-    <h1>Shop All Planets</h1>
-    <h2>View our entire selection of planets for sale. Select a planet for more details.</h2>
+    <link rel="stylesheet" href="css/allplanets.css" TYPE="text/css">
+    <main>
+        <h1>Shop All Planets</h1>
+        <h2>View our entire selection of planets for sale. Select a planet for more details.</h2>
 
-    <section class="planetGrid">
-        <figure class="planet1">
-            <img src="images/planets/planet4.jpg">
-            <figcaption>Planet 1</figcaption>
-        </figure>
+        <section class="planetGrid">
+            <figure class="planet1">
+                <img src="images/planets/planet1.png">
+                <figcaption><a href="fakeplanet1.php">Fake Planet 1</a></figcaption>
+            </figure>
 
-        <figure class="planet2">
-            <img src="images/planets/planet2.jpg">
-            <figcaption>Planet 2</figcaption>
-        </figure>
+            <figure class="planet2">
+                <img src="images/planets/jupiter.png">
+                <figcaption><a href="jupiter.php">Jupiter</a></figcaption>
+            </figure>
 
-        <figure class="planet3">
-            <img src="images/planets/planet1.png">
-            <figcaption>Planet 3</figcaption>
-        </figure>
+            <figure class="planet3">
+                <img src="images/planets/planet2.jpg">
+                <figcaption><a href="fakeplanet2.php">Fake Planet 2</a></figcaption>
+            </figure>
 
-        <figure class="planet4">
-            <img src="images/planets/planet3.jpg">
-            <figcaption>Planet 4</figcaption>
-        </figure>
+            <figure class="planet4">
+                <img src="images/planets/saturn.png">
+                <figcaption><a href="saturn.php">Saturn</a></figcaption>
+            </figure>
 
-        <figure class="planet5">
-            <img src="images/planets/planet1.png">
-            <figcaption>Planet 5</figcaption>
-        </figure>
+            <figure class="planet5">
+                <img src="images/planets/planet3.jpg">
+                <figcaption><a href="fakeplanet3.php">Fake Planet 3</a></figcaption>
+            </figure>
 
-        <figure class="planet6">
-            <img src="images/planets/planet4.jpg">
-            <figcaption>Planet 6</figcaption>
-        </figure>
+            <figure class="planet6">
+                <img src="images/planets/pluto.png">
+                <figcaption><a href="pluto.php">Pluto</a></figcaption>
+            </figure>
 
-        <figure class="planet7">
-            <img src="images/planets/planet2.jpg">
-            <figcaption>Planet 7</figcaption>
-        </figure>
+            <figure class="planet7">
+                <img src="images/planets/planet4.jpg">
+                <figcaption><a href="fakeplanet4.php">Fake Planet 4</a></figcaption>
+            </figure>
 
-        <figure class="planet8">
-            <img src="images/planets/planet4.jpg">
-            <figcaption>Planet 8</figcaption>
-        </figure>
+            <figure class="planet8">
+                <img src="images/planets/venus.png">
+                <figcaption><a href="venus.php">Venus</a></figcaption>
+            </figure>
 
-        <figure class="planet9">
-            <img src="images/planets/planet3.jpg">
-            <figcaption>Planet 9</figcaption>
-        </figure>
-    </section>
-</main>
+            <figure class="planet9">
+                <img src="images/planets/planet5.jpg">
+                <figcaption><a href="fakeplanet5.php">Fake Planet 5</a></figcaption>
+            </figure>
+        </section>
+    </main>
 
 
 <?php include "footer.php"; ?>
