2aa970cc7eed0669597606f23c7dafaf165827e1
zachrossi <57643983+zachrossi@users.noreply.github.com>
2019-12-05 15:01:13 -0500

    Updated planet list

--- hobbiton.php
+++ hobbiton.php
@@ -169,11 +169,11 @@ function verifyAlphaNum($testString) {
                     <select name="selPlanet" id="selPlanet" required>
                         <option value='hobbiton'>Hobbiton</option>
                         <option value='jupiter'>Jupiter</option>
-                        <option value='fakeplanet2'>Fake Planet 2</option>
+                        <option value='jeffbezos'>Jeffbezos</option>
                         <option value='saturn'>Saturn</option>
-                        <option value='fakeplanet3'>Fake Planet 3</option>
+                        <option value='odysseus'>Odysseus</option>
                         <option value='pluto'>Pluto</option>
-                        <option value='fakeplanet4'>Fake Planet 4</option>
+                        <option value='winturn'>Winturn</option>
                         <option value='venus'>Venus</option>
                         <option value='fakeplanet5'>Fake Planet 5</option>
                     </select>
