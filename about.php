b96571fe9a32e2bbb35c3e765f1fe60ef10c37b1
jldeland <57643954+jldeland@users.noreply.github.com>
2019-12-04 20:20:43 -0500

    Update about.php (should be complete)

--- about.php
+++ about.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <!--
 about.php
-
 add
 -image
 -->
@@ -11,23 +10,66 @@ include ("nav.php");
 ?>
 <main>
     <article>
-        <section>
-            <h1>About Us</h1>
-            <p>Insert text about maybe 500ish words</p>
-        </section>
-        <!--Picture of logo, maybe-->
-        <section>
-            <figure>
-                <figcaption></figcaption>
-            </figure>
-        </section>
-        <section>
-            <h2>Contact Us</h2>
-            <address>67 Star Avenue, Moon Colony 6</address>
-            <p>Phone Number: (666) 666 6661</p>
-            <p>Email: contact@planetbay.com</p>
-        </section>
+        <h1>The History of PlanetBay</h1>
+        <p>Only six years after graduating from the University of Vermont, three young men with an entrepreneurial
+            spirit and a dream to one day combine their passions of real estate and space exploration started a small
+            company called Extraterrestrial Property Management (EPM). With numerous recent advancements in space
+            travel by NASA, tens of thousands of planets in the Milky Way Galaxy had already been discovered and
+            meticulously analyzed. Additionally, traveling to planets became as inexpensive as an airplane ticket
+            was in the early 2000s. Tyler Eisenmenger, Zachary Rossi, and Joshua Deland saw an opportunity to
+            take advantage of the vast expanse of planets. The three developed an intricate legal process for an
+            individual or organization to purchase a planet and have complete rights to the resources on their
+            planet. After receiving approval from United Nations to greenlight the "Extraterrestrial Property
+            Management" website and mobile application, orders came rushing in, mainly from the elite 0.001% and
+            from Global 500 companies who wanted to take advantage of external resources. With the influx of
+            orders, EPM signed an agreement for NASA to become the official provider of spaceships for real estate
+            tours and resource transportation. After only eight months on the market, multi-billionaire Jeff Bezos
+            acquired EPM as a subsidiary of Amazon.com Inc. and renamed the company to PlanetBay, which is what it
+            is known as today. Since the acquisition, PlanetBay has grown by a record-breaking 15,391%. Despite
+            growing competition, we remain the world's largest distributor of extraterrestrial property.
+        </p>
+        <h2>Our Mission</h2>
+        <p>Our foremost purpose may appear grandiose, but we believe it is the most vital: to push humankind
+        beyond the stars and into new eras of technological advancement and prosperity. With our vast selection
+        of planets with unfathomable resources and features, we as a species have entered an era of unending
+        opportunities.</p>
+
     </article>
+
+    <section class="founders">
+        <h2>Founding Members</h2>
+        <figure>
+            <img src="images/misc/josh.jpg" alt="Joshua Deland">
+            <figcaption>Joshua Deland</figcaption>
+        </figure>
+        <figure>
+            <img src="images/misc/zach.jpg" alt="Zachary Rossi">
+            <figcaption>Zachary Rossi</figcaption>
+        </figure>
+        <figure>
+            <img src="images/misc/tyler.png" alt="Tyler Eisenmenger">
+            <figcaption>Tyler Eisenmenger</figcaption>
+        </figure>
+    </section>
+
+    <section class="contactUs">
+        <h2>Contact Us</h2>
+        <address>Rocket Rd. Hawthorne, CA 90250</address>
+        <p>Phone Number: (401) 742-7620</p>
+        <p>Email: zross@uvm.edu</p>
+    </section>
+
+    <section class="map">
+        <h2>Headquarters</h2>
+        <iframe
+                width="600"
+                height="450"
+                frameborder="0" style="border:0"
+                src="https://www.google.com/maps/embed/v1/place?key=AIzaSyDUvn4YzOPHoRy504fiaz7LSHVTp5L-GLc
+    &q=SpaceX+Hawthorne,CA" allowfullscreen>
+        </iframe>
+    </section>
+
     <?php
     include ("footer.php");
     ?>
