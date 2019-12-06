11a11c054648f38e38a958b6efb81255eece1837
TylerEisenmenger <57907283+TylerEisenmenger@users.noreply.github.com>
2019-12-06 10:01:19 -0500

    Update footer.php

--- footer.php
+++ footer.php
@@ -3,10 +3,8 @@ footer
 -->
 <footer class="footer_paragraph">
     <ul>
-        <li><a href="index.php">Home</a></li>
-        <li><a href="deals.php">Today's Deals</a></li>
+        <li><a href="index.php">Home</a></li>   
         <li><a href="products.php">Shop Planets</a></li>
-        <li><a href="contest.php">Contest</a></li>
         <li><a href="about.php">About</a></li>
         <p class='names'>Tyler Eisenmenger CS008A</p>
         <p class='names'>Zachary Rossi CS008A</p>
