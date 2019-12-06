9ac408e94ef37433efb99589667930cf610f7e2a
TylerEisenmenger <57907283+TylerEisenmenger@users.noreply.github.com>
2019-12-06 09:58:56 -0500

    Update nav.php

--- nav.php
+++ nav.php
@@ -12,16 +12,7 @@
             print '">';
             print '<a href="index.php">Home</a>';
             print '</li>';
-
-            //Today's Deals
-            print '<li class="';
-            if ($path_parts['filename'] == "deals") {
-                print 'activePage';
-            }
-            print '">';
-            print '<a href="form.php">Today\'s Deals</a>';
-            print '</li>';
-
+            
             //Products
             print '<li class="';
             if ($path_parts['filename'] == "allplanets") {
@@ -30,24 +21,14 @@
             print '">';
             print '<a href="allplanets.php">Shop Planets</a>';
             print '</li>';
-            
-            //Contest
-            print '<li class="';
-            if ($path_parts['filename'] == "contest") {
-                print 'activePage';
-            }
-            print '">';
-            print '<a href="contest.php">Contest</a>';
-            print '</li>';
-            
-            
+                      
             //About
             print '<li class="';
             if ($path_parts['filename'] == "about") {
                 print 'activePage';
             }
             print '">';
-            print '<a href="about.php">About</a>';
+            print '<a href="about_us.php">About</a>';
             print '</li>';
             ?>
             
