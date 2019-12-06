47336ec1367f1c86804259ca933052922943f42e
jldeland <57643954+jldeland@users.noreply.github.com>
2019-12-06 10:57:02 -0500

    Update footer

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@ footer
 <footer class="footer_paragraph">
     <ul>
         <li><a href="index.php">Home</a></li>   
-        <li><a href="products.php">Shop Planets</a></li>
+        <li><a href="allplanets.php">Shop Planets</a></li>
         <li><a href="about.php">About</a></li>
         <p class='names'>Tyler Eisenmenger CS008A</p>
         <p class='names'>Zachary Rossi CS008A</p>
