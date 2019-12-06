6ac97f6c6dbb520e7a6f12a209305c0a9ca4aa00
TylerEisenmenger <57907283+TylerEisenmenger@users.noreply.github.com>
2019-12-06 10:10:57 -0500

    Update nav.php

--- nav.php
+++ nav.php
@@ -28,7 +28,7 @@
                 print 'activePage';
             }
             print '">';
-            print '<a href="about_us.php">About</a>';
+            print '<a href="about.php">About</a>';
             print '</li>';
             ?>
             
